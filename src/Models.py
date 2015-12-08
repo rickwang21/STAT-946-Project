14bd5284a5f81c974b5a252c540664f6bafa1fe1
Ri Wang <rickwang21@gmail.com>
2015-12-08 15:44:09 -0500

    s

--- src/Models.py
+++ src/Models.py
@@ -5,8 +5,9 @@ import theano
 import theano.tensor as T
 import lasagne
 
-def Create_simple_LSTM(N_HIDDEN=1000,GRAD_CLIP=100,vocab=80000,layer=4,LEARNING_RATE=0.01):
-    l_in = lasagne.layers.InputLayer(shape=(None, None, 300))
+def Create_simple_LSTM(N_HIDDEN=1000,GRAD_CLIP=100,vocab=80002,layer=4,LEARNING_RATE=0.01,input_var=None):
+    target_var = T.ivector('targets')
+    l_in = lasagne.layers.InputLayer(shape=(None, None, 300),input_var=input_var)
     network = lasagne.layers.LSTMLayer(
             l_in, N_HIDDEN, grad_clipping=GRAD_CLIP,
             nonlinearity=lasagne.nonlinearities.tanh)
@@ -24,10 +25,13 @@ def Create_simple_LSTM(N_HIDDEN=1000,GRAD_CLIP=100,vocab=80000,layer=4,LEARNING_
     target_values = T.ivector('target_out')
     network_output = lasagne.layers.get_output(l_out)
     cost = T.nnet.categorical_crossentropy(network_output,target_values).mean()
+    test_acc = T.mean(T.eq(T.argmax(network_output, axis=1), target_values),dtype=theano.config.floatX)
     all_params = lasagne.layers.get_all_params(l_out)
-    updates = lasagne.updates.adagrad(cost, all_params, LEARNING_RATE)
-    train = theano.function([l_in.input_var, target_values], cost, updates=updates, allow_input_downcast=True)
-    compute_cost = theano.function([l_in.input_var, target_values], cost, allow_input_downcast=True)
-    probs = theano.function([l_in.input_var],network_output,allow_input_downcast=True)
-    return l_out, train, probs
+    updates = lasagne.updates.nesterov_momentum(
+            cost, all_params, learning_rate=LEARNING_RATE, momentum=0.9)
+    train = theano.function([input_var, target_values], cost, updates=updates, allow_input_downcast=True)
+    compute_cost = theano.function([input_var, target_values], cost, allow_input_downcast=True)
+    probs = theano.function([input_var],network_output,allow_input_downcast=True)
+    val_fn = theano.function([input_var, target_values], [cost, test_acc])
+    return l_out, train, val_fn,network_output
 
--- src/helpers.py
+++ src/helpers.py
@@ -5,34 +5,68 @@ import theano.tensor as T
 from random import shuffle
 
 def find_vector(word,vectors):
-    if(len(vectors[vectors['word'] == word])==1):
-        temp = vectors[vectors['word'] == word]['vector'].str.split().as_matrix()
-        return temp
+    if word in vectors:
+        temp = vectors[word]
+        return True, temp
     else:
-        return 'NULL'
+        #print(word)
+	#with open('missing','ab') as f: f.write(word+'\n')
+        return False, 'NULL'
 
 def convert_to_vector(sentences,french_set,english_set):
-    french = sentences['french'].str.split(' ')
+    #print(sentences)
+    #print(sentences.shape)
+    french = sentences['french'].str.split()
     length = french.shape[0]
-    english = sentences['english'].str.split(' ')
+    english = sentences['english'].str.split()
+    length_en = english.shape[0]
     french = french.apply(lambda list:list_to_word_vectors(list,french_set)).values.tolist()
-    english = english.apply(lambda list:list_to_word_vectors(list,english_set)).values.tolist()
+    english = english.apply(lambda list:word_to_idx(list,english_set)).values.tolist()
     french_mat = np.ones([length,french[0].shape[0],300])
     i = 0
     for sentence in french:
-        if(sentence.shape[0]==french[0].shape[0]):
-            french_mat[i] = sentence
+        french_mat[i] = sentence
         i += 1
-    return french_mat,english
+    i = 0
+    english_mat = np.ones([length_en,english[0].shape[1]])
+    for sentence in english:
+        english_mat[i] = sentence
+        i += 1
+    #print(english_mat.shape)
+    #print(french_mat.shape)
+    return [french_mat.astype('float32'),english_mat.astype('int')]
 
 def list_to_word_vectors(list_of_strings,dictionary):
     temp = np.ones((300,len(list_of_strings)),dtype=np.float32)
     i = 0
     for word in list_of_strings:
-        temp_result = find_vector(word,dictionary)
-        if(temp_result!='NULL'):
-            temp[:,i] = temp_result[0]
+        tr,temp_result = find_vector(word,dictionary)
+        if(tr):
+            temp[:,i] = temp_result
         i += 1
     temp = temp.transpose()
     return temp
 
+def word_to_idx(list_of_strings,dictionary):
+    temp = np.ones((1,len(list_of_strings)+1),dtype=np.int)*80001
+    i = 0
+    for word in list_of_strings:
+        if word in dictionary:
+            temp[0,i] = dictionary[word]
+        i += 1
+    temp[0,len(list_of_strings)] = 80002
+    return temp
+
+def shift_to_input(batch,word,ix_to_vector):
+   temp_eng = np.copy(batch[1])
+   temp_fr = np.copy(batch[0])
+   new_fr = np.ones((temp_eng.shape[0],temp_fr.shape[1]+1,300))
+   #print(new_fr.shape," ",temp_eng.shape)
+   for i in range(temp_eng.shape[0]):
+       if temp_eng[i,0] in ix_to_vector:
+            new_fr[i,temp_fr.shape[1]] = ix_to_vector[temp_eng[i,0]]
+       #else:
+            #print(temp_eng[i,0])
+   new_fr[:,:temp_fr.shape[1],:] = temp_fr[:,:,:]
+   return new_fr.astype('float32'),temp_eng[:,1:]
+        
